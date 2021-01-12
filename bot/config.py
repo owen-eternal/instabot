d953fb6eade054e1c3a705387bb448339f5ef7ad
Owen Phakade <68030544+owen-eternal@users.noreply.github.com>
2021-01-12 13:24:34 +0200

    Update config.py
    changed credentials class into a dictionary to host multiple bots/
    changed IG_handle to accept a list of insta accounts.

--- bot/config.py
+++ bot/config.py
@@ -1,22 +1,43 @@
 import os
 
-#CREDENTIALS
+# List of bot accounts
+
+
 class Credentials(object):
-    
-    BOT_EMAIL = os.environ.get('BOT_EMAIL')
-    BOT_PASSWORD = os.environ.get('DB_PASSWORD')
+    # load credentials from your environment variables
+    ig_accounts = [
+        {
+            'id': 1,
+            'name': 'xxxx',
+            'username': 'xxxx',
+            'password': 'xxxx'
+        },
+        {
+            'id': 2,
+            'name': 'xxxx',
+            'username': 'xxxx',
+            'password': 'xxxx'
+        },
+        {
+            'id': 3,
+            'name': 'xxxx',
+            'username': 'xxxx',
+            'password': 'xxxx'
+        },
+    ]
 
-#BOT SETTINGS
-class Settings(Credentials):
 
-    USER = 'Kolumbusbeats'
-    LIKES_PP = 3 
-    COMMENTS_PP = 0
+# BOT SETTINGS
+class Configurations():
+
+    IG_HANDLE = ['ausi_bree']  # A LIST OF NSTAGRAM HANDLES
+    LIKES_PP = 0  # NUMBER OF POSTS TO LIKE
+    COMMENTS_PP = 0  # NUMBER OF POST TO COMMENT ON
 
     COMM_LIST = [
-                    'Interesting feed, I love it!',
-                    'The best post I have seen today',
-                    'wonderful',
-                    'see why you are my favourite instagramer??',
-                    'This is beautiful. I am looking forward to seeing more posts from you. '
-                ]
+        'Interesting feed, I love it!',
+        'The best post I have seen today',
+        'wonderful',
+        'see why you are my favourite instagramer??',
+        'This is beautiful. I am looking forward to seeing more posts from you. '
+    ]
